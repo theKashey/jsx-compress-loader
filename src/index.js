50fb54901ea08bf13045d5eab03a72dd1cd2d9d9
Anton Korzunov <thekashey@gmail.com>
2018-10-27 23:41:03 +1100

    support React.Fragment

--- src/index.js
+++ src/index.js
@@ -11,13 +11,16 @@ function transform(source, map) {
     const appendText = `          
     var __react_jsx__ = require('react');
     var _J$X_ = (__react_jsx__.default || __react_jsx__).createElement;
+    var _J$F_ = (__react_jsx__.default || __react_jsx__).Fragment;
   `;
 
     const newSource = source
-    // ts and es6 "react"
+      // ts and es6 "react"
       .replace(/React\.createElement\(/g, '_J$X_(')
+      .replace(/React\.Fragment\(/g, '_J$F_(')
       // transpiled react
-      .replace(/_react2\.default\.createElement\(/g, '_J$X_(');
+      .replace(/_react2\.default\.createElement\(/g, '_J$X_(')
+      .replace(/_react2\.default\.Fragment\(/g, '_J$F_(');
 
     if (!this.sourceMap || !map) {
       return this.callback(null, [
