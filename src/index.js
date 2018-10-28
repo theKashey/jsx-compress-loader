f59148e0149a4cde87f90ad2278654e069294877
Anton Korzunov <thekashey@gmail.com>
2018-10-28 18:56:17 +1100

    handle more cases

--- src/index.js
+++ src/index.js
@@ -6,21 +6,31 @@ function transform(source, map) {
   if (this.cacheable) {
     this.cacheable();
   }
-  if (source.indexOf('React.createElement') >= 0) {
+  if (
+    source.indexOf('React.createElement') >= 0 ||
+    source.indexOf('React__default.createElement') >= 0 ||
+    source.indexOf('_react2.default.createElement') >= 0 ||
+    0
+  ) {
     const separator = '\n\n;';
     const appendText = `          
+    // ${source.indexOf('React.createElement')};
     var __react_jsx__ = require('react');
     var _J$X_ = (__react_jsx__.default || __react_jsx__).createElement;
     var _J$F_ = (__react_jsx__.default || __react_jsx__).Fragment;
   `;
 
     const newSource = source
-      // ts and es6 "react"
+    // ts and es6 "react"
       .replace(/React\.createElement\(/g, '_J$X_(')
       .replace(/React\.Fragment\(/g, '_J$F_(')
       // transpiled react
       .replace(/_react2\.default\.createElement\(/g, '_J$X_(')
-      .replace(/_react2\.default\.Fragment\(/g, '_J$F_(');
+      .replace(/_react2\.default\.Fragment\(/g, '_J$F_(')
+      //
+      .replace(/React__default\.createElement\(/g, '_J$X_(')
+      .replace(/React__default\.Fragment\(/g, '_J$F_(');
+
 
     if (!this.sourceMap || !map) {
       return this.callback(null, [
